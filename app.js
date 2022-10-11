66dc59acacde0f25bf2c25e936f48d2d41fde2c3
onur koc <onurkottz@gmail.com>
2022-10-11 12:29:43 +0300

    database again

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const pageControllers = require("./controllers/pageControllers");
 const app = express();
 mongoose
   .connect(
-    'mongodb+srv://onurkoc:KODV0sr95W8z8r33@cluster0.d6cuxty.mongodb.net/?retryWrites=true&w=majority'
+    'mongodb+srv://onurkoc:49mq1tQAjyivVuCU@cluster0.7aekmmt.mongodb.net/?retryWrites=true&w=majority'
   )
   .then(() => {
     console.log("Database connected.");
